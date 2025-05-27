ef4f4f5df8e621682edd827535d3c9fce1997f1c
Hesbon2 <hesbonmakori15@gmail.com>
2025-05-27 08:53:10 +0300

    Fix SocketIO

--- websocket.js
+++ websocket.js
@@ -11,7 +11,11 @@ class SocketServer {
                 methods: ['GET', 'POST'],
                 credentials: true
             },
-            path: '/socket.io'
+            path: '/socket.io',
+            transports: ['websocket', 'polling'],
+            allowEIO3: true,
+            pingTimeout: 60000,
+            pingInterval: 25000
         });
 
         this.clients = new Map();
